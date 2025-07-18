47b68dd17b49e6730473b2255c3239ba5e8b32b5
Tech Garden <techgardencode@gmail.com>
2025-07-18 15:24:02 -0400

    feat: add actuator

--- src/main/java/gg/techgarden/houseparty/party/config/SecurityConfig.java
+++ src/main/java/gg/techgarden/houseparty/party/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .requestMatchers("/**").authenticated())
                 .cors(Customizer.withDefaults())
