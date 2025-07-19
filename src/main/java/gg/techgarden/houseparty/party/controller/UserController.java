7d589c3da1e561b2f939c7164b41da34b647896e
Tech Garden <techgardencode@gmail.com>
2025-07-19 12:51:24 -0400

    feat: init user profile on load

--- src/main/java/gg/techgarden/houseparty/party/controller/UserController.java
+++ src/main/java/gg/techgarden/houseparty/party/controller/UserController.java
@@ -2,9 +2,9 @@ package gg.techgarden.houseparty.party.controller;
 
 
 import gg.techgarden.houseparty.party.model.common.UserInfoDto;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import gg.techgarden.houseparty.party.persistence.entity.UserInfo;
+import gg.techgarden.houseparty.party.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @GetMapping("/profile")
-    public UserInfoDto getProfile(JwtAuthenticationToken auth) {
-        return UserInfoDto.builder()
-            .id(auth.getToken().getClaimAsString(StandardClaimNames.SUB))
-            .username(auth.getToken().getClaimAsString(StandardClaimNames.PREFERRED_USERNAME))
-            .firstName(auth.getToken().getClaimAsString(StandardClaimNames.GIVEN_NAME))
-            .lastName(auth.getToken().getClaimAsString(StandardClaimNames.FAMILY_NAME))
-            .name(auth.getToken().getClaimAsString(StandardClaimNames.NAME))
-            .email(auth.getToken().getClaimAsString(StandardClaimNames.EMAIL))
-            .roles(auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
-            .build();
+    public UserInfo getProfile() {
+        return userService.getProfile();
     }
 }
